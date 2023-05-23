bc3227bd0a0ca3c45d8deeb6d8fb26275a8836f0
20CS074Mandar <20CS074@charusat.edu.in>
2023-05-23 10:30:18 +0530

    Added Index.php file

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+$host= 'mariadb';
+$user= 'root';
+$pass='root';
+
+$conn = new mysqli($host,$user,$pass);
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+} else {
+   echo "Connected to MySQL server successfully!";
+}
+>?
