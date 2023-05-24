e3327228a533261f0a57d1cba68b2fa2c7442353
mvshah10 <mvshah1011@gmail.com>
2023-05-24 14:32:28 +0530

    updated

--- tests/indexTest.php
+++ tests/indexTest.php
@@ -4,7 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase {
     public function testAdd() {
-        echo "Hello world";
+        $user = new User(18, 'John');
+
+        $this->assertSame('John', $user->name);
+        $this->assertSame(18, $user->age);
     }
 }
 ?>
